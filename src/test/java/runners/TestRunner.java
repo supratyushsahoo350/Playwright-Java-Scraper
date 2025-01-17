93d34dfcfdb1db0273a63c0edb1e50a7ccb554ed
supratyushsahoo350 <110294092+supratyushsahoo350@users.noreply.github.com>
2025-01-18 00:18:16 +0530

    Update TestRunner.java

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "C:/Users/prati/WebstormProjects/Java-Scaper/src/test/resources/features", // Absolute path to your feature files
+        features = "src/test/resources/features", // Absolute path to your feature files
         glue = {"steps"}, // Package containing step definitions
         plugin = {"pretty", "html:target/cucumber-reports.html"} // Optional: Generate readable test reports
 )
